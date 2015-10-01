cf888d1ff7f527aa4cc704758a4dadc0e8a1989c
Nguyen Hoang Vu <vunh@greenglobal.vn>
2015-10-01 17:26:36 +0700

    Update file CommentModuleSeeder #1

---
+++ packages/Comments/database/seeds/CommentModuleSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+class CommentModuleSeeder extends Seeder
+{
+    /**
+     * Run the shopping cart seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
