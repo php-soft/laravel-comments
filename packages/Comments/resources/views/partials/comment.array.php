e26c673eabd14f12db1a08b698ae5fabadb5c042
Nguyen Hoang Vu <vunh@greenglobal.vn>
2015-10-09 11:13:44 +0700

    Using camelCase in comment template #15

--- packages/Comments/resources/views/partials/comment.array.php
+++ packages/Comments/resources/views/partials/comment.array.php
@@ -2,11 +2,11 @@
 
 $this->extract($comment, [
     'id',
-    'user_id',
     'url',
     'content',
 ]);
+$this->set('userId', $comment->user_id);
 
-$this->set('created at', date('c', strtotime($comment->created_at)));
+$this->set('createdAt', date('c', strtotime($comment->created_at)));
 
-$this->set('updated at', date('c', strtotime($comment->updated_at)));
+$this->set('updatedAt', date('c', strtotime($comment->updated_at)));
